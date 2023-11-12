69732b0d9aad04cf51df350cdccd7048af62c592
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2023-11-12 06:48:32 -0300

    configuracao dos models task, user e category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Task;
 
 class Category extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title', 'color',
+        'user_id'
+    ];
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
