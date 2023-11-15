2d3bc28f5dbd9f8d1aa0c40dbb7e977104a0a792
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2023-11-15 10:54:33 -0300

    configuração inicial dos templates

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoApp</title>
+    {{-- FONTES --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    funciona pfv
+
+</body>
+
+</html>
