66121da5fbc2a02c2d5a6b64709fd8c8f4d7be32
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2023-11-19 09:35:03 -0300

    molde do grafico

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -10,11 +10,39 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 
 <body>
-    <link rel="stylesheet" href="/assets/css/style.css">
-    funciona pfv
+    <div class="container">
+        <div class="sidebar">
+            logo
+        </div>
+        <div class="content">
+            <nav>
+                <a href=""class="btn btn-primary">Criar Tarefa</a>
+            </nav>
+            <main>
+                <section class="graph">
+                    <div class="graph_header">
+                        <h2>Progresso do dia</h2>
+                        <hr class="LinhaHeader">
+                        data
+                    </div>
+                    <div class="graph_header-subtitle">Tarefa: <b> 3/6 </b></div>
+                    <div class="graph-placeholder"></div>
+                    <p class="graph_header-tasks_left">Restam 3 tarefas para serem realizadas</p>
+                </section>
+                <section class="list">
+                    <div class="list-header">
+                        <select class="list_header-select">
+                            <option value="1">Todas as Tarefas</option>
+                        </select>
+                    </div>
+                </section>
+            </main>
+        </div>
+    </div>
 
 </body>
 
