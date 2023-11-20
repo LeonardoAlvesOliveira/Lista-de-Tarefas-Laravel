45f4d5f06c0e7f19bc5098ed4e0c79639fd19ce2
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2023-11-20 16:37:05 -0300

    componente

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+<x-layout>
+    <section class="graph">
+        <div class="graph_header">
+            <h2>Progresso do dia</h2>
+            <div class="graph_header-line"></div>
+            <div class="graph_header-date">
+                <img src="/assets/images/icon-prev.png" alt="">13 de Dez
+                <img src="/assets/images/icon-next.png" alt="">
+            </div>
+        </div>
+        <div class="graph_header-subtitle">Tarefa: <b> 3/6 </b></div>
+        <div class="graph-placeholder"></div>
+        <div class="tasks_left_footer">
+            <img src="/assets/images/icon-info.png" alt="">
+            Restam 3 tarefas para serem realizadas
+        </div>
+
+    </section>
+    <section class="list">
+        <div class="list-header">
+            <select class="list_header-select">
+                <option value="1">Todas as Tarefas</option>
+            </select>
+        </div>
+        <div class="task_list">
+            <div class="task">
+                <div class="title">
+                    <input type="checkbox">
+                    <h1>Titulo da tarefa</h1>
+                </div>
+                <div class="priority">
+                    <div class="sphere"></div>
+                    <div class="task_title">Titulo da tarefa</div>
+                </div>
+                <div class="actions">
+                    <a href="">
+                        <img src="/assets/images/icon-edit.png" alt="">
+                    </a>
+                    <a href="">
+                        <img src="/assets/images/icon-delete.png" alt="">
+                    </a>
+                </div>
+            </div>
+    </section>
+    </main>
+</x-layout>
